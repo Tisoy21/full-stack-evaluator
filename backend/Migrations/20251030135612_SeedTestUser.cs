dee99f5fe267711f38ab22ae14916acea3fbf002
Jom-Laptop <jerome092197@gmail.com>
2025-10-31 00:05:41 +0800

    chore: migration and seeds

---
+++ backend/Migrations/20251030135612_SeedTestUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace task_manager_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedTestUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "PasswordHash" },
+                values: new object[] { 1, "testuser@example.com", "test1234" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
